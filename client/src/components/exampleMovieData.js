2fc98fc3e4465b0f96046a2e121c7baea39f3000
William Tucker <william.tucker29@gmail.com>
2024-02-07 18:37:15 -0600

    Hard coded data to test at beginning of app build.

---
+++ client/src/components/exampleMovieData.js
@@ -0,0 +1,10 @@
+
+var exampleMovieData = [
+  { title: 'Mean Girls' },
+  { title: 'Hackers' },
+  { title: 'The Grey' },
+  { title: 'Sunshine' },
+  { title: 'Ex Machina' },
+];
+
+export default exampleMovieData;
