f4f3bdba94d12e9515794db28589e86ed40a3dc5
William Tucker <william.tucker29@gmail.com>
2024-02-07 18:31:36 -0600

    Build out App.jsx file. Created Add Bar for adding movies to the list. Created Nav Bar for searching for movies. Created MovieList component that holds each movie added.

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,7 +1,76 @@
 import React from 'react';
+import MovieList from './MovieList.jsx';
+import MovieListEntry from './MovieListEntry.jsx';
+import Search from './Search.jsx';
+import AddMovie from './AddMovie.jsx';
+import exampleMovieData from './exampleMovieData.js';
+const { useState } = React;
 
-const App = (props) => (
-  <div>Hello World!</div>
-);
+const App = (props) => {
+  const [movieList, setMovieList] = useState([]);
+  const [filteredList, setFilteredList] = useState([]);
+  const [watchedFilter, setWatchedFilter] = useState(false);
+  const [watchedList, setWatchedList] = useState([]);
+  const [toWatchList, setToWatchList] = useState([]);
+
+  const handleWatchedToggle = (title) => {
+    // Toggle if movie watched when clicked
+    const updatedList = movieList.map(movie => {
+      if (movie.title === title) {
+        return { ...movie, watched: !movie.watched};
+      }
+      return movie;
+    });
+    setWatchedList(updatedList.filter(movie => movie.watched));
+    setToWatchList(updatedList.filter(movie => !movie.watched));
+    setMovieList(updatedList);
+  };
+
+  // const handleToWatchToggle = () => {
+  //   setWatchedFilter(false);
+  //   const toWatchMovies = movieList.filter(movie => !movie.watched);
+  //   setToWatchList(toWatchMovies);
+  // };
+
+  const handleAddClick = (query) => {
+    // create function to add movie to list
+    var addedMovie = {title: query, watched: false};
+    var list = [...movieList, addedMovie];
+
+    // Use setMovieList on value we set the added movie to
+    setMovieList(list);
+    setWatchedList(list);
+    setToWatchList(list);
+  };
+
+  const handleSearchClick = (query) => {
+    if (query.length > 0) {
+      const filteredMovies = movieList.filter(movie =>
+        movie.title.toLowerCase().includes(query.toLowerCase())
+      );
+      if (filteredMovies.length === 0) {
+        alert('No movie by that name found in the list.');
+      }
+      setFilteredList(filteredMovies);
+    } else {
+      setFilteredList([]);
+    }
+  };
+
+  return (
+    <div>
+      <h1 className="page-title">Movie List</h1>
+      <div className="addbar">
+        <AddMovie onAddClick={handleAddClick}/>
+      </div>
+      <div className="navbar">
+        <Search onSearchClick={handleSearchClick} />
+      </div>
+      <div className="list">
+        <MovieList movies={movieList} filtered={filteredList} watchedMovies={watchedList} toWatchMovies={toWatchList }onWatchedToggle={handleWatchedToggle}/>
+      </div>
+    </div>
+  )
+};
 
 export default App;
