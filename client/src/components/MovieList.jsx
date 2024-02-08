739b4d8058f9d574f68fc7de0d95f8596bda2957
William Tucker <william.tucker29@gmail.com>
2024-02-08 12:10:12 -0600

    Added prop for onWatchedToggle to cascade a function into the MovieListEntry component.

--- client/src/components/MovieList.jsx
+++ client/src/components/MovieList.jsx
@@ -3,31 +3,50 @@ import MovieListEntry from './MovieListEntry.jsx';
 import App from './App.jsx';
 const { useState } = React;
 
-var MovieList = ({ movies, filtered, onWatchedToggle}) => {
-  console.log('MovieList props:', movies);
+var MovieList = ({ filtered, onWatchedToggle}) => {
+  //console.log('MovieList props:', movies);
   console.log('FilteredList props', filtered);
 
-  if (movies.length > 0 && filtered.length === 0) {
-    return (
-      <div className="movie-list">
-        {movies.map(movie => (
-          <MovieListEntry key={movie.title} movie={movie} onWatchedToggle={onWatchedToggle}/>
-        ))}
-      </div>
-    );
-  } else if (movies.length === 0 && filtered.length === 0) {
-    return (
-      <div className="movie-list">No Movies Currently Added</div>
-    );
-  } else if (filtered.length > 0) {
-    return (
-      <div className="filtered-movie-list">
-        {filtered.map(movie => (
-          <MovieListEntry key={movie.title} movie={movie} onWatchedToggle={onWatchedToggle}/>
-        ))}
-      </div>
-    );
-  }
+  // let displayList = [];
+  // if (filtered.length > 0) {
+  //   displayList = filtered;
+  // } else if (watchedMovies.length > 0) {
+  //   displayList = watchedMovies;
+  // } else if (toWatchMovies.length > 0) {
+  //   displayList = toWatchMovies;
+  // } else {
+  //   displayList = movies;
+  // }
+
+  return (
+    <div className="movie-list">
+      {filtered.map(movie => (
+        <MovieListEntry key={movie.title} movie={movie} onWatchedToggle={onWatchedToggle} />
+      ))}
+    </div>
+  );
+
+  // if (movies.length > 0 && filtered.length === 0) {
+  //   return (
+  //     <div className="movie-list">
+  //       {movies.map(movie => (
+  //         <MovieListEntry key={movie.title} movie={movie} onWatchedToggle={onWatchedToggle}/>
+  //       ))}
+  //     </div>
+  //   );
+  // } else if (movies.length === 0 && filtered.length === 0) {
+  //   return (
+  //     <div className="movie-list">No Movies Currently Added</div>
+  //   );
+  // } else if (filtered.length > 0) {
+  //   return (
+  //     <div className="filtered-movie-list">
+  //       {filtered.map(movie => (
+  //         <MovieListEntry key={movie.title} movie={movie} onWatchedToggle={onWatchedToggle}/>
+  //       ))}
+  //     </div>
+  //   );
+  // }
 }
 
 export default MovieList;
