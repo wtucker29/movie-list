3c2da782da5257b31626f93d0ff071186edd43bd
William Tucker <william.tucker29@gmail.com>
2024-02-16 12:46:28 -0600

    Created file connections between all the new server side files.

--- server/index.js
+++ server/index.js
@@ -1,7 +1,27 @@
 const express = require('express');
+const db = require('./db');
+
+// Middleware
+const morgan = require('morgan');
+const cors = require('cors');
+
+// Router
+const router = require('./routes.js')
+
 const app = express();
 const PORT = 3000 || process.env.PORT;
 
+// Connecting to database happens in ./db/index.js
+
+// Logging and parsing
+app.use(morgan('dev'));
+app.use(cors());
+app.use(express.json());
+
+// Set up routes
+app.use('/lists', router);
+
+// Serve client files
 app.use(express.static('client/dist'));
 
 app.listen(PORT, () => {
