c3250a56dbfa42d784c2b48f15f7cd4a08ad4b5f
William Tucker <william.tucker29@gmail.com>
2024-02-16 13:38:40 -0600

    Built out get and post requests in the server controller file.

--- server/controllers/movielist.js
+++ server/controllers/movielist.js
@@ -1,7 +1,27 @@
 const models = require('../models');
 
 module.exports = {
-  get: function(req, res) {};
+  get: function(req, res) {
+    models.movielist.getAll((err, movies) => {
+      if (err) {
+        res.status(500).send('Error retrieving movies');
+        return;
+      }
+      res.status(200).send(movies);
+    })
+  },
 
-  post: function(req, res) {};
+  post: function(req, res) {
+    // console.log('req.body from controllers: ', req.body);
+    const { title, watched } = req.body;
+    const movieInput = { title, watched };
+    // console.log('movieInput from controllers: ', movieInput);
+    models.movielist.create(movieInput, (err, result) => {
+      if (err) {
+        res.status(500).send('Error adding movie to list');
+        return;
+      }
+      res.status(201).send('Movie added to list successfully');
+    });
+  }
 };
