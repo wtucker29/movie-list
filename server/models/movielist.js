c7e25521162f2f195b0d0035f05ed378621d391f
William Tucker <william.tucker29@gmail.com>
2024-02-16 13:39:15 -0600

    Built out getAll and create functions inside server models.

--- server/models/movielist.js
+++ server/models/movielist.js
@@ -1,7 +1,29 @@
 const db = require('../db');
 
 module.exports = {
-  getAll: function(callback) {};
+  getAll: function(callback) {
+    const getMoviesQuery = 'SELECT * FROM movies';
+    const getMoviesQueryArgs = [];
 
-  create: function(movieInput, callback) {};
+    db.query(getMoviesQuery, getMoviesQueryArgs, (err, result) => {
+      if (err) {
+        callback(err, null);
+        return;
+      }
+      callback(null, result);
+    });
+  },
+
+  create: function(movieInput, callback) {
+    // console.log('movieInput from models: ', movieInput);
+    const { title, watched } = movieInput;
+    const createMovieQuery = 'INSERT INTO movies (title, watched) VALUES (?, ?)';
+    db.query(createMovieQuery, [title, watched], (err, result) => {
+      if (err) {
+        callback(err, null);
+        return;
+      }
+      callback(null, result);
+    });
+  }
 };
