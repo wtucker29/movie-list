7dea4eb4fa72a31bf28db78af723e27f3125906b
William Tucker <william.tucker29@gmail.com>
2024-02-16 18:30:57 -0600

    Updated class names for css.

--- client/src/components/MovieList.jsx
+++ client/src/components/MovieList.jsx
@@ -5,18 +5,7 @@ const { useState } = React;
 
 var MovieList = ({ filtered, onWatchedToggle}) => {
   //console.log('MovieList props:', movies);
-  console.log('FilteredList props', filtered);
-
-  // let displayList = [];
-  // if (filtered.length > 0) {
-  //   displayList = filtered;
-  // } else if (watchedMovies.length > 0) {
-  //   displayList = watchedMovies;
-  // } else if (toWatchMovies.length > 0) {
-  //   displayList = toWatchMovies;
-  // } else {
-  //   displayList = movies;
-  // }
+  //console.log('FilteredList props', filtered);
 
   return (
     <div className="movie-list">
@@ -25,28 +14,6 @@ var MovieList = ({ filtered, onWatchedToggle}) => {
       ))}
     </div>
   );
-
-  // if (movies.length > 0 && filtered.length === 0) {
-  //   return (
-  //     <div className="movie-list">
-  //       {movies.map(movie => (
-  //         <MovieListEntry key={movie.title} movie={movie} onWatchedToggle={onWatchedToggle}/>
-  //       ))}
-  //     </div>
-  //   );
-  // } else if (movies.length === 0 && filtered.length === 0) {
-  //   return (
-  //     <div className="movie-list">No Movies Currently Added</div>
-  //   );
-  // } else if (filtered.length > 0) {
-  //   return (
-  //     <div className="filtered-movie-list">
-  //       {filtered.map(movie => (
-  //         <MovieListEntry key={movie.title} movie={movie} onWatchedToggle={onWatchedToggle}/>
-  //       ))}
-  //     </div>
-  //   );
-  // }
 }
 
 export default MovieList;
--- client/src/components/MovieListEntry.jsx
+++ client/src/components/MovieListEntry.jsx
@@ -9,8 +9,10 @@ var MovieListEntry = ({movie, onWatchedToggle}) => {
 
   return (
     <div className="movie-list-entry">
-      <span className="movie-list-entry-title">{movie.title}</span>
-      <button className="movie-list-entry-btn" onClick={handleWatchedToggle}>{movie.watched ? 'Watched' : 'Not Watched'}</button>
+      <div className="movie-list-entry-content">
+        <span className="movie-list-entry-title">{movie.title}</span>
+        <button className={movie.watched ? 'button-3 watched' : 'button-3 to-watch'} onClick={handleWatchedToggle}>{movie.watched ? 'Watched' : 'Not Watched'}</button>
+      </div>
     </div>
   );
 }
